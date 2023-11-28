365915526609a5e17d5f9c8fc50e40af9f1f3312
Calix-Mingyu-Kim <calixkim58@gmail.com>
2023-11-27 22:58:14 -0500

    modification to index.mjs

--- index.mjs
+++ index.mjs
@@ -2,10 +2,24 @@ import { exec } from 'child_process';
 
 export const handler = async (event, context) => {
     try {
-        exec("nvm use 18", (error, stdout, stderr) => {
+        exec("npm install -g n", (error, stdout, stderr) => {
             if (error) {
-                console.error('Error in nvm use 18: ', error)
-                return context.fail('Error in nvm use 18')
+                console.error('Error in npm install -g n: ', error)
+                return context.fail('Error in npm install -g n')
+            } 
+        })
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+        };
+    }
+    try {
+        exec("n 18", (error, stdout, stderr) => {
+            if (error) {
+                console.error('Error in n 18: ', error)
+                return context.fail('Error in n 18')
             } 
         })
     } catch (error) {
