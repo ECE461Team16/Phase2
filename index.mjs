4688051f7fd7974da984a01cfacc5e6e32551662
Calix-Mingyu-Kim <calixkim58@gmail.com>
2023-11-29 12:08:43 -0500

    update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,56 +1,44 @@
+import { promisify } from 'util';
 import { exec } from 'child_process';
+
+const execAsync = promisify(exec);
 
 export const handler = async (event, context) => {
     const url = JSON.parse(JSON.stringify(event)).URL;
 
     try {
-        exec('rm -rf /tmp/*', (error, stdout, stderr) => {  //remove /tmp file
-            if (error) {
-              console.error(`exec error: ${error}`);
-            }
-          });
+        // Asynchronously remove files in /tmp directory
+        await execAsync('rm -rf /tmp/*');
+    } catch (error) {
+        console.error('Error removing files in /tmp:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+        };
+    }
 
+    try {
         // Construct the command to run the Bash script
         const command = `./run URL_FILE ${url}`;
 
         // Variable to store the output
         let scriptOutput = '';
+
         // Execute the command
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error('Error running Bash script:', error);
-                return context.fail('Error running Bash script');
-            } else {
-                // Capture the output
-                scriptOutput = stdout;
+        const { stdout, stderr } = await execAsync(command);
 
-                console.log('Bash script output:', scriptOutput);
-                // return scriptOutput;
-            }
-        });
-        // // Log the script output as it happens
-        // childProcess.stdout?.on('data', (data) => {
-        //     console.log('Bash script output:', data);
-        // });
+        // Capture the output
+        scriptOutput = stdout;
 
-        // childProcess.stderr?.on('data', (data) => {
-        //     console.error('Bash script error:', data);
-        // });
+        console.log('Bash script output:', scriptOutput);
 
-        // // Optionally, wait for the process to exit
-        // await new Promise((resolve) => {
-        //     childProcess.on('exit', () => {
-        //         resolve();
-        //     });
-        // });
-        
         // Return the output as part of the response
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Bash script executed successfully', output: scriptOutput }),
         };
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error running Bash script:', error);
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Internal Server Error' }),
